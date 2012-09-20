f4757477b29227b19ef4a8117a71554e62a0b952
Ilias-Dimitrios Vrachnis <vid@transifex.com>
2012-09-20 19:32:35 +0300

    Add path to an exec resource.

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,29 +1,32 @@
 class sun-jdk::debian
 {
-    
-    $pressed_filename = "sun-jdk6.pressed"
-    $pressed_dir = "/var/cache/debconf/"  
-    $pressed_file = "${pressed_dir}/${pressed_filename}"
-    $package_name = "sun-java6-jdk"
 
-    file { "$pressed_file":
-        source => "puppet:///sun-jdk/dpkg/$pressed_filename",
-        ensure => present
-    }
+  $pressed_filename = "sun-jdk6.pressed"
+  $pressed_dir = "/var/cache/debconf/"
+  $pressed_file = "${pressed_dir}/${pressed_filename}"
+  $package_name = "sun-java6-jdk"
 
-    package { "$package_name":
-        ensure => "latest",
-        responsefile => "$pressed_file",
-        require => File["$pressed_file"]
-    }
+  file { "$pressed_file":
+    source => "puppet:///modules/sun-jdk/dpkg/$pressed_filename",
+    ensure => present
+  }
 
-    package { "java-common":
-        ensure => "latest",
-        require => Package["$package_name"]
-    }
- 
-    exec { "update-java-alternatives-sun-jdk-6":
-        command => "update-java-alternatives --jre --set java-6-sun",
-        require => Package["java-common"]
-    }
+  package { "$package_name":
+    ensure       => latest,
+    responsefile => "$pressed_file",
+    require      => File["$pressed_file"]
+  }
+
+  package { "java-common":
+    ensure  => latest,
+    require => Package["$package_name"],
+    notify  => Exec['update-java-alternatives-sun-jdk-6'];
+  }
+
+  exec { 'update-java-alternatives-sun-jdk-6':
+    command     => 'update-java-alternatives --jre --set java-6-sun',
+    path        => '/sbin:/bin:/usr/sbin:/usr/bin',
+    refreshonly => true,
+    require     => Package["java-common"];
+  }
 }
